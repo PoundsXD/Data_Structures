0165115517708452e2dc6b79e10a6fbc28d5d326
Brian Wheeler <PoundsXD@localhost.localdomain>
2017-11-27 11:05:17 -0500

    Created basic frame work. yep that's it.

---
+++ bst.py
@@ -0,0 +1,26 @@
+"""."""
+
+class BinarySearchTree():
+    """."""
+
+    def __init__():
+        """."""
+        self.
+
+    def insert():
+        """."""
+
+    def search():
+        """."""
+
+    def size():
+        """."""
+
+    def depth():
+        """."""
+
+    def contains():
+        """."""
+
+    def balance():
+        """."""
