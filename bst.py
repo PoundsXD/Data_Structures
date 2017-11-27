c2003c0d148611249418efa300c8fc596af7ca72
Brian Wheeler <PoundsXD@localhost.localdomain>
2017-11-27 11:54:11 -0500

    Started to work through some of the core logic.

--- bst.py
+++ bst.py
@@ -3,12 +3,17 @@
 class BinarySearchTree():
     """."""
 
-    def __init__():
+    def __init__(self):
         """."""
-        self.
+        self.core_dictionary = {}
 
-    def insert():
+    def insert(self, item):
         """."""
+        if 0 not in self.core_dictionary.keys():
+            self.core_dictionary.setdefault(0, item = {})
+        for key in self.core_dictionary.keys():
+            if self.core_dictionary[key] is not (key * 2):
+                self.core_dictionary.setdefault(key, )
 
     def search():
         """."""
